62aecebe8cdb2400e9a5b0265a8938fcfe27ef9e
Maryam Ariyan <maariyan@microsoft.com>
2020-05-14 15:37:55 -0700

    nit cleanup - Remove unused properties on *FormatterOptions - ScopeProvider to be set outside of logger ctor

--- src/libraries/Microsoft.Extensions.Logging.Console/src/Formatter/SystemdLogFormatterOptions.cs
+++ src/libraries/Microsoft.Extensions.Logging.Console/src/Formatter/SystemdLogFormatterOptions.cs
@@ -17,11 +17,6 @@ namespace Microsoft.Extensions.Logging.Console
         public bool IncludeScopes { get; set; }
 
         /// <summary>
-        /// Disables colors when <see langword="true" />.
-        /// </summary>
-        public bool DisableColors { get; set; }
-        
-        /// <summary>
         /// Gets or sets value indicating the minimum level of messaged that would get written to <c>Console.Error</c>.
         /// </summary>
         public LogLevel LogToStandardErrorThreshold { get; set; } = LogLevel.None;
